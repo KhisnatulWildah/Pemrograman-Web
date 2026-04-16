5656ae49147cd190ee08458c2ae7e09b5b070b8c
KhisnatulWildah <khisnawildah@gmail.com>
2026-04-16 21:36:28 +0700

    Add files via upload

---
+++ content/pemweb_unsiq.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Pertemuan 2 HTML Dasar</title>
+</head>
+<body>
+
+    <table border="1" width="100%">
+        <tr>
+            <td width="120">
+                <img src="LOGO UNSIQ.jpeg" width="120">
+            </td>
+            <td>
+                <h1>Selamat Datang di Mata Kuliah Pemrograman Web</h1>
+                <h2>Pertemuan 2: HTML Dasar</h2>
+            </td>
+        </tr>
+    </table>
+
+    <p align="center">
+        Mata kuliah ini memperkenalkan dasar-dasar pemrograman web menggunakan HTML, CSS, dan JavaScript.
+    </p>
+    <p align="center">
+        Pada pertemuan ini, kita fokus pada struktur dan elemen dasar HTML.
+    </p>
+
+    <hr>
+
+    <p>
+        <b>HTML</b> adalah bahasa markup yang digunakan untuk membuat struktur halaman web.
+        Dengan HTML, kita bisa menampilkan teks, gambar, dan link secara terorganisir.
+        Untuk informasi lengkap dari HTML dapat kunjungi link berikut
+        <a href="https://www.w3schools.com/html/" target="_blank">Penjelasan HTML</a>
+    </p>
+
+    <h2>Topik Utama Pertemuan Ini:</h2>
+    <ul>
+        <li>Struktur HTML</li>
+        <li>Heading dan Paragraf</li>
+        <li>List (Unordered dan Ordered)</li>
+        <li>Link dan Gambar</li>
+        <li>Tabel</li>
+    </ul>
+
+    <h2>Langkah Praktikum:</h2>
+    <ol>
+        <li>Buat file index.html</li>
+        <li>Tulis kode HTML</li>
+        <li>Simpan dan buka di browser
+            <ul>
+                <li>buka xampp</li>
+                <li>nyalakan server</li>
+                <li>buka browser dengan url localhost/...</li>
+            </ul>
+        </li>
+        <li>Verifikasi hasil</li>
+    </ol>
+</body>
+</html>
---
+++ content/pertemuan4.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tabel Data</title>
+</head>
+<body style="font-family: Arial; margin: 40px;">
+    <!-- ================== DATA MAHASISWA ================= -->
+    <table border="1" cellpadding="10" cellspacing="0" style="border-collapse: collapse;" width="30%">
+        <tr>
+            <th colspan="5">Data Mahasiswa</th>
+        </tr>
+        <tr>
+            <th>No</th>
+            <th>Nama</th>
+            <th>Alamat</th>
+            <th>Umur</th>
+            <th>Foto</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Rizky</td>
+            <td>Jl. Merdeka No. 123</td>
+            <td>20</td>
+            <td align="center">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/4/48/Rizky_Ridho_-_53615135042.jpg" width="80">
+            </td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Siti</td>
+            <td>Jl. Sudirman No. 456</td>
+            <td>22</td>
+            <td align="center">
+                <img src="https://awsimages.detik.net.id/community/media/visual/2023/05/11/sosok-wilda-siti-nurfadhilah-sugandi-wanita-yang-pakai-hijab-pertama-di-asean-games-2.png?w=1200" width="80">
+            </td>
+        </tr>
+    </table>
+    <br><br>
+
+    <!-- ================= DAFTAR SMARTPHONE ================= -->
+    <table border="1" cellpadding="10" cellspacing="0" style="border-collapse: collapse;" width="55%">
+        <tr>
+            <th colspan="4">Daftar Smartphone</th>
+        </tr>
+        <tr>
+            <th>No</th>
+            <th>Nama Brand</th>
+            <th>Gambar</th>
+            <th>Keterangan</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Apple</td>
+            <td align="center">
+                <img src="https://www.digimap.co.id/cdn/shop/files/0788-APPMYWV3ID-A-1.jpg?v=1734067389" width="80">
+            </td>
+            <td>
+                <div>iPhone 16 Pro Max</div>
+                <div style="border-top: 1px solid black; margin-top:5px; padding-top:5px;">
+                    Dengan desain titanium yang memukau. Kontrol Kamera, Dolby Vision 4K 120 fps, dan chip A18 Pro.
+                 </div>
+                <div style="border-top: 1px solid black; margin-top:5px; padding-top:5px;">
+                    Rp 20.000.000
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Samsung</td>
+            <td align="center">
+                <img src="https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full/catalog-image/109/MTA-177756283/samsung_hp_samsung_s24_ultra_12-256_full01_e3da06fa.jpg" width="80">
+            </td>
+            <td>
+                <div>Samsung Galaxy S24 Ultra</div>
+                <div style="border-top: 1px solid black; margin-top:5px; padding-top:5px;">
+                    HP premium dengan layar besar, S Pen, kamera canggih, dan performa sangat mewah.
+                 </div>
+                <div style="border-top: 1px solid black; margin-top:5px; padding-top:5px;">
+                    Rp 18.000.000
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Xiaomi</td>
+            <td align="center">
+                <img src="https://images.tokopedia.net/img/cache/700/VqbcmM/2024/6/9/b6c8414a-cea3-4403-9c04-541e05efd6db.jpg.webp" width="80">
+            </td>
+            <td>
+                <div>Xiaomi 12 8/256</div>
+                <div style="border-top: 1px solid black; margin-top:5px; padding-top:5px;">
+                    HP flagship dengan performa kencang, desain elegan, dan harga lebih terjangkau.
+                 </div>
+                <div style="border-top: 1px solid black; margin-top:5px; padding-top:5px;">
+                    Rp 8.000.000
+                </div>
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
