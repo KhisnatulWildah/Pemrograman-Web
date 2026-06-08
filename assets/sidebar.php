20bd347d1d653451da495573f2e7e052f66ab90f
KhisnatulWildah <khisnawildah@gmail.com>
2026-06-08 22:00:05 +0700

    Add files via upload

---
+++ assets/sidebar.php
@@ -0,0 +1,128 @@
+<aside class="app-sidebar bg-body-secondary shadow" data-bs-theme="dark">
+  <!--begin::Sidebar Brand-->
+  <div class="sidebar-brand">
+    <a href="./index.html" class="brand-link">
+      <img
+        src="./assets/img/AdminLTELogo.png"
+        alt="AdminLTE Logo"
+        class="brand-image opacity-75 shadow"
+      />
+      <span class="brand-text fw-light">AdminLTE 4</span>
+    </a>
+  </div>
+  <!--end::Sidebar Brand-->
+
+  <!--begin::Sidebar Wrapper-->
+  <div class="sidebar-wrapper">
+    <nav class="mt-2">
+
+      <!--begin::Sidebar Menu-->
+      <ul
+        class="nav sidebar-menu flex-column"
+        data-lte-toggle="treeview"
+        role="navigation"
+        aria-label="Main navigation"
+        data-accordion="false"
+        id="navigation"
+      >
+
+        <!-- Dashboard -->
+        <li class="nav-item menu-open">
+          <a href="#" class="nav-link active">
+            <i class="nav-icon bi bi-speedometer"></i>
+            <p>
+              Dashboard
+              <i class="nav-arrow bi bi-chevron-right"></i>
+            </p>
+          </a>
+
+          <ul class="nav nav-treeview">
+
+            <li class="nav-item">
+              <a href="./index.html" class="nav-link active">
+                <i class="nav-icon bi bi-circle"></i>
+                <p>Dashboard v1</p>
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a href="./index2.html" class="nav-link">
+                <i class="nav-icon bi bi-circle"></i>
+                <p>Dashboard v2</p>
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a href="./index3.html" class="nav-link">
+                <i class="nav-icon bi bi-circle"></i>
+                <p>Dashboard 3</p>
+              </a>
+            </li>
+
+          </ul>
+        </li>
+
+        <!-- Perkuliahan -->
+        <li class="nav-item menu-open">
+          <a href="#" class="nav-link">
+            <i class="nav-icon bi bi-book"></i>
+            <p>
+              Perkuliahan
+              <i class="nav-arrow bi bi-chevron-right"></i>
+            </p>
+          </a>
+
+          <ul class="nav nav-treeview">
+
+            <li class="nav-item">
+              <a href="pertemuan2.php" class="nav-link">
+                <i class="nav-icon bi bi-circle"></i>
+                <p>Pertemuan 2</p>
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a href="pertemuan3.php" class="nav-link">
+                <i class="nav-icon bi bi-circle"></i>
+                <p>Pertemuan 3</p>
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a href="pertemuan4.php" class="nav-link">
+                <i class="nav-icon bi bi-circle"></i>
+                <p>Pertemuan 4</p>
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a href="pertemuan5.php" class="nav-link">
+                <i class="nav-icon bi bi-circle"></i>
+                <p>Pertemuan 5</p>
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a href="pertemuan6.php" class="nav-link">
+                <i class="nav-icon bi bi-circle"></i>
+                <p>Pertemuan 6</p>
+              </a>
+            </li>
+
+            <li class="nav-item">
+              <a href="pertemuan7.php" class="nav-link active">
+                <i class="nav-icon bi bi-circle"></i>
+                <p>Pertemuan 7</p>
+              </a>
+            </li>
+
+          </ul>
+        </li>
+
+      </ul>
+      <!--end::Sidebar Menu-->
+
+    </nav>
+  </div>
+  <!--end::Sidebar Wrapper-->
+</aside>
